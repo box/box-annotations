056913cb68981a8ae978c430182ebdec988f4387
Sumedha Pramod <spramod@box.com>
2017-09-28 10:40:24 -0700

    Migrating annotations code into repo

---
+++ src/icons/icons.js
@@ -0,0 +1,21 @@
+import ANNOTATION from './annotation_24px.svg';
+import ANNOTATION_HIGHLIGHT_COMMENT from './annotation_highlight_comment_24px.svg';
+import PLACED_ANNOTATION from './placed_annotation_24px.svg';
+import DROP_DOWN from './arrow_drop_down_24px.svg';
+import DROP_UP from './arrow_drop_up_24px.svg';
+import DELETE from './delete_24px.svg';
+import HIGHLIGHT from './highlight_24px.svg';
+import CLOSE from './close.svg';
+import DRAW_SAVE from './draw_save.svg';
+import DRAW_DELETE from './draw_delete.svg';
+
+export const ICON_ANNOTATION = ANNOTATION;
+export const ICON_HIGHLIGHT_COMMENT = ANNOTATION_HIGHLIGHT_COMMENT;
+export const ICON_PLACED_ANNOTATION = PLACED_ANNOTATION;
+export const ICON_DROP_DOWN = DROP_DOWN;
+export const ICON_DROP_UP = DROP_UP;
+export const ICON_DELETE = DELETE;
+export const ICON_HIGHLIGHT = HIGHLIGHT;
+export const ICON_CLOSE = CLOSE;
+export const ICON_DRAW_SAVE = DRAW_SAVE;
+export const ICON_DRAW_DELETE = DRAW_DELETE;
