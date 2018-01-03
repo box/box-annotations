box/box-annotations

commit 00989ac9df89d9f87fb9114d306ab3bc9d243bc2

Chore: Move threadmap entirely to mode controllers  (#71)
* Chore: Render/hide annotations from controller
* Chore: Remove references to annotator.threads
* Chore: Remove util.addThreadFromMap/removeThreadFromMap
* Fix: annotator.scrollIntoView()
* Fix: rendering/hiding of draw annotations
* Remove unused hideAnnotations() and hideAnnotationsOnPage()
* Chore: Renaming render methods

src/controllers/DrawingModeController.js
@@ -354,6 +354,23 @@ class DrawingModeController extends AnnotationModeController {
     }
 
     /**
+     * Renders annotations from memory for a specified page.
+     *
+     * @inheritdoc
+     * @private
+     * @param {number} pageNum - Page number
+     * @return {void}
+     */
+    renderPage(pageNum) {
+        if (!this.threads || !this.threads[pageNum]) {
+            return;
+        }
+
+        const pageThreads = this.threads[pageNum].all() || [];
+        pageThreads.forEach((thread) => thread.show());
+    }
+
+    /**
      * Deselect a saved and selected thread
      *
      * @private

commit f39608de89193b1fc3eeec26f5d4e137e0c5fade

Fix: Add safety check when getting annotation layer context
- Fixes issue where only the first canvas layer was rendered on page
  load and any subsequent layers would render on click (Caused in
https://github.com/box/box-annotations/pull/71)
- Ensures canvas contexts are properly cleared on page render
- Removes unneeded showHighlightsOnPage() method

src/constants.js
@@ -158,7 +158,6 @@ export const CONTROLLER_EVENT = {
     exit: 'annotationmodeexit',
     register: 'registerthread',
     unregister: 'unregisterthread',
-    showHighlights: 'showhighlights',
     bindDOMListeners: 'binddomlisteners',
     unbindDOMListeners: 'unbinddomlisteners'
 };

src/controllers/DrawingModeController.js
@@ -13,6 +13,7 @@ import {
     SELECTOR_BOX_PREVIEW_BASE_HEADER,
     SELECTOR_ANNOTATION_DRAWING_HEADER,
     CLASS_ANNNOTATION_DRAWING_BACKGROUND,
+    CLASS_ANNOTATION_LAYER_DRAW,
     DRAW_BORDER_OFFSET,
     CLASS_ACTIVE,
     CLASS_ANNOTATION_MODE,
@@ -328,7 +329,7 @@ class DrawingModeController extends AnnotationModeController {
         }
 
         const location = this.annotator.getLocationFromEvent(event, TYPES.point);
-        if (!location || Object.keys(this.threads).length === 0) {
+        if (!location || Object.keys(this.threads).length === 0 || !this.threads[location.page]) {
             return;
         }
 
@@ -366,6 +367,10 @@ class DrawingModeController extends AnnotationModeController {
      * @return {void}
      */
     renderPage(pageNum) {
+        // Clear context if needed
+        const pageEl = this.annotatedElement.querySelector(`[data-page-number="${pageNum}"]`);
+        util.clearCanvas(pageEl, CLASS_ANNOTATION_LAYER_DRAW);
+
         if (!this.threads || !this.threads[pageNum]) {
             return;
         }

src/controllers/HighlightModeController.js
@@ -1,5 +1,6 @@
 import AnnotationModeController from './AnnotationModeController';
-import { THREAD_EVENT, CONTROLLER_EVENT } from '../constants';
+import { clearCanvas } from '../util';
+import { THREAD_EVENT, CONTROLLER_EVENT, CLASS_ANNOTATION_LAYER_HIGHLIGHT } from '../constants';
 
 class HighlightModeController extends AnnotationModeController {
     /**
@@ -16,7 +17,7 @@ class HighlightModeController extends AnnotationModeController {
     handleThreadEvents(thread, data) {
         switch (data.event) {
             case THREAD_EVENT.threadCleanup:
-                this.emit(CONTROLLER_EVENT.showHighlights, thread.location.page);
+                this.renderPage(thread.location.page);
                 break;
             default:
         }
@@ -46,6 +47,26 @@ class HighlightModeController extends AnnotationModeController {
     enter() {
         this.emit(CONTROLLER_EVENT.unbindDOMListeners); // Disable other annotations
         this.bindListeners(); // Enable mode
+    }
+
+    /**
+     * Renders annotations from memory for a specified page.
+     *
+     * @inheritdoc
+     * @private
+     * @param {number} pageNum - Page number
+     * @return {void}
+     */
+    renderPage(pageNum) {
+        // Clear context if needed
+        const pageEl = this.annotatedElement.querySelector(`[data-page-number="${pageNum}"]`);
+        clearCanvas(pageEl, CLASS_ANNOTATION_LAYER_HIGHLIGHT);
+
+        if (!this.threads) {
+            return;
+        }
+
+        super.renderPage(pageNum);
     }
 }
 

src/doc/DocAnnotator.js
@@ -970,32 +970,6 @@ class DocAnnotator extends Annotator {
     }
 
     /**
-     * Shows highlight annotations for the specified page by re-drawing all
-     * highlight annotations currently in memory for the specified page.
-     *
-     * @private
-     * @param {number} page Page to draw annotations for
-     * @return {void}
-     */
-    showHighlightsOnPage(page) {
-        // Clear context if needed
-        const pageEl = this.annotatedElement.querySelector(`[data-page-number="${page}"]`);
-        const annotationLayerEl = pageEl.querySelector(`.${CLASS_ANNOTATION_LAYER_HIGHLIGHT}`);
-        if (annotationLayerEl) {
-            const context = annotationLayerEl.getContext('2d');
-            context.clearRect(0, 0, annotationLayerEl.width, annotationLayerEl.height);
-        }
-
-        if (this.plainHighlightEnabled) {
-            this.modeControllers[TYPES.highlight].renderPage(page);
-        }
-
-        if (this.commentHighlightEnabled) {
-            this.modeControllers[TYPES.highlight_comment].renderPage(page);
-        }
-    }
-
-    /**
      * Helper to remove a Rangy highlight by deleting the highlight in the
      * internal highlighter list that has a matching ID. We can't directly use
      * the highlighter's removeHighlights since the highlight could possibly
@@ -1036,9 +1010,6 @@ class DocAnnotator extends Annotator {
                     document.getSelection().removeAllRanges();
                     this.createHighlightDialog.hide();
                 }
-                break;
-            case CONTROLLER_EVENT.showHighlights:
-                this.showHighlightsOnPage(data.data);
                 break;
             case CONTROLLER_EVENT.bindDOMListeners:
                 if (isCreateDialogVisible) {

src/doc/docUtil.js
@@ -360,11 +360,11 @@ export function getContext(pageEl, annotationLayerClass) {
     annotationLayerEl = scaleCanvas(pageEl, annotationLayerEl);
 
     const textLayerEl = pageEl.querySelector('.textLayer');
+    const canvasWrapperEl = pageEl.querySelector('.canvasWrapper');
     if (textLayerEl) {
         pageEl.insertBefore(annotationLayerEl, textLayerEl);
-    } else {
+    } else if (canvasWrapperEl) {
         // Ensure the annotation layer is added before the point annotations
-        const canvasWrapperEl = pageEl.querySelector('.canvasWrapper');
         canvasWrapperEl.appendChild(annotationLayerEl);
     }
     return annotationLayerEl.getContext('2d');

src/util.js
@@ -835,3 +835,22 @@ export function createCommentTextNode(annotationText) {
 
     return textEl;
 }
+
+/**
+ * Clears the specified canvas context
+ *
+ * @param {HTMLElement} pageEl The DOM element for the current page
+ * @param {HTMLElement} layerClass The annotation canvas layer CSS class
+ * @return {void}
+ */
+export function clearCanvas(pageEl, layerClass) {
+    const annotationLayerEl = pageEl.querySelector(`.${layerClass}`);
+    if (!annotationLayerEl) {
+        return;
+    }
+
+    const context = annotationLayerEl.getContext('2d');
+    if (context) {
+        context.clearRect(0, 0, annotationLayerEl.width, annotationLayerEl.height);
+    }
+}

src/__tests__/util-test.js
@@ -37,7 +37,8 @@ import {
     generateBtn,
     addThreadToMap,
     removeThreadFromMap,
-    createCommentTextNode
+    createCommentTextNode,
+    clearCanvas
 } from '../util';
 import {
     STATES,
@@ -799,6 +800,36 @@ describe('util', () => {
             const textEl = createCommentTextNode(text);
 
             expect(textEl.classList.contains(CLASS_ANNOTATION_COMMENT_TEXT)).to.be.true;
+        });
+    });
+
+    describe('clearCanvas()', () => {
+        beforeEach(() => {
+            stubs.layerEl = {
+                width: 500,
+                height: 500,
+                getContext: () => {}
+            };
+            stubs.layerMock = sandbox.mock(stubs.layerEl);
+
+            stubs.context = {
+                clearRect: () => {}
+            };
+            stubs.contextMock = sandbox.mock(stubs.context);
+        });
+
+        it('should do nothing if the annotation layer does not exist', () => {
+            stubs.contextMock.expects('clearRect').never();
+            clearCanvas(document.createElement('div'), 'anything');
+        });
+
+        it('should clear the specified annotation layer', () => {
+            const pageEl = {
+                querySelector: sandbox.stub().returns(stubs.layerEl)
+            };
+            stubs.layerMock.expects('getContext').returns(stubs.context);
+            stubs.contextMock.expects('clearRect').once();
+            clearCanvas(pageEl, 'anything');
         });
     });
 });

src/controllers/__tests__/DrawingModeController-test.js
@@ -453,7 +453,11 @@ describe('controllers/DrawingModeController', () => {
         };
 
         beforeEach(() => {
+            controller.annotatedElement = document.createElement('div');
+            controller.annotatedElement.setAttribute('data-page-number', 1);
+
             stubs.threadMock = sandbox.mock(thread);
+            sandbox.stub(util, 'clearCanvas');
         });
 
         it('should do nothing if no threads exist or none are on the specified page', () => {
@@ -463,6 +467,7 @@ describe('controllers/DrawingModeController', () => {
             controller.threads = {};
             controller.registerThread(thread);
             controller.renderPage(2);
+            expect(util.clearCanvas).to.be.calledTwice;
         });
 
         it('should render the annotations on every page', () => {
@@ -470,6 +475,7 @@ describe('controllers/DrawingModeController', () => {
             controller.registerThread(thread);
             stubs.threadMock.expects('show').once();
             controller.renderPage(1);
+            expect(util.clearCanvas).to.be.called;
         });
     });
 

src/controllers/__tests__/HighlightModeController-test.js
@@ -7,7 +7,8 @@ import {
     CLASS_ANNOTATION_MODE,
     THREAD_EVENT,
     STATES,
-    CONTROLLER_EVENT
+    CONTROLLER_EVENT,
+    CLASS_ANNOTATION_LAYER_HIGHLIGHT
 } from '../../constants';
 
 let controller;
@@ -18,7 +19,11 @@ describe('controllers/HighlightModeController', () => {
     beforeEach(() => {
         controller = new HighlightModeController();
         sandbox.stub(controller, 'emit');
-        stubs.thread = { location: { page: 1 } };
+        stubs.thread = {
+            location: { page: 1 },
+            show: () => {}
+        };
+        stubs.threadMock = sandbox.mock(stubs.thread);
     });
 
     afterEach(() => {
@@ -30,9 +35,10 @@ describe('controllers/HighlightModeController', () => {
     describe('handleThreadEvents()', () => {
         it('should unregister thread on threadCleanup', () => {
             sandbox.stub(controller, 'unregisterThread');
+            sandbox.stub(controller, 'renderPage');
             controller.handleThreadEvents(stubs.thread, { event: THREAD_EVENT.threadCleanup, data: {} });
             expect(controller.unregisterThread).to.be.called;
-            expect(controller.emit).to.be.calledWith(CONTROLLER_EVENT.showHighlights, 1);
+            expect(controller.renderPage).to.be.calledWith(1);
         });
     });
 
@@ -65,6 +71,30 @@ describe('controllers/HighlightModeController', () => {
             controller.enter();
             expect(controller.emit).to.be.calledWith(CONTROLLER_EVENT.unbindDOMListeners);
             expect(controller.bindListeners).to.be.called;
+        });
+    });
+
+    describe('renderPage()', () => {
+        beforeEach(() => {
+            controller.annotatedElement = document.createElement('div');
+            controller.annotatedElement.setAttribute('data-page-number', 1);
+
+            sandbox.stub(util, 'clearCanvas');
+        });
+
+        it('should do nothing if no threads exist', () => {
+            stubs.threadMock.expects('show').never();
+            controller.renderPage(1);
+            expect(util.clearCanvas).to.be.called;
+        });
+
+        it('should render the annotations on the specified page', () => {
+            controller.threads = {
+                1: { '123abc': stubs.thread }
+            };
+            stubs.threadMock.expects('show').once();
+            controller.renderPage(1);
+            expect(util.clearCanvas).to.be.called;
         });
     });
 });

src/doc/__tests__/DocAnnotator-test.js
@@ -1386,83 +1386,6 @@ describe('doc/DocAnnotator', () => {
         });
     });
 
-    describe('showHighlightsOnPage()', () => {
-        beforeEach(() => {
-            stubs.getContext = sandbox.stub();
-            stubs.clearRect = sandbox.stub();
-
-            annotator.annotatedElement = {
-                querySelector: () => {},
-                getContext: () => {},
-                clearRect: () => {}
-            };
-            stubs.mock = sandbox.mock(annotator.annotatedElement);
-
-            annotator.modeControllers = {
-                'highlight': {
-                    renderPage: () => {}
-                },
-                'highlight-comment': {
-                    renderPage: () => {}
-                }
-            };
-            stubs.highlightMock = sandbox.mock(annotator.modeControllers['highlight']);
-            stubs.commentMock = sandbox.mock(annotator.modeControllers['highlight-comment']);
-        });
-
-        afterEach(() => {
-            annotator.annotatedElement = document.querySelector('.annotated-element');
-        });
-
-        it('should not call clearRect or getContext if there is already an annotationLayerEl', () => {
-            stubs.mock.expects('querySelector').returns({ querySelector: () => {} });
-            stubs.mock.expects('clearRect').never();
-            stubs.mock.expects('getContext').never();
-
-            annotator.showHighlightsOnPage(0);
-        });
-
-        it('should not call clearRect or getContext if there is not an annotationLayerEl', () => {
-            stubs.mock.expects('querySelector').returns(annotator.annotatedElement);
-            stubs.mock.expects('querySelector').returns(undefined);
-            stubs.mock.expects('clearRect').never();
-            stubs.mock.expects('getContext').never();
-
-            annotator.showHighlightsOnPage(0);
-        });
-
-        it('should call clearRect or getContext if there is an annotationLayerEl', () => {
-            stubs.mock.expects('querySelector').returns(annotator.annotatedElement);
-            stubs.mock.expects('querySelector').returns(annotator.annotatedElement);
-            stubs.mock.expects('getContext').returns(annotator.annotatedElement);
-            stubs.mock.expects('clearRect');
-
-            annotator.showHighlightsOnPage(0);
-        });
-
-        it('show all plain highlights on the page after clearing', () => {
-            stubs.mock.expects('querySelector').returns(annotator.annotatedElement);
-            stubs.mock.expects('querySelector').returns(annotator.annotatedElement);
-            stubs.mock.expects('getContext').returns(annotator.annotatedElement);
-            stubs.mock.expects('clearRect');
-
-            annotator.plainHighlightEnabled = true;
-            stubs.highlightMock.expects('renderPage').withArgs(0);
-            annotator.showHighlightsOnPage(0);
-        });
-
-        it('show all the highlight comments on the page after clearing', () => {
-            stubs.mock.expects('querySelector').returns(annotator.annotatedElement);
-            stubs.mock.expects('querySelector').returns(annotator.annotatedElement);
-            stubs.mock.expects('getContext').returns(annotator.annotatedElement);
-            stubs.mock.expects('clearRect');
-
-            annotator.commentHighlightEnabled = true;
-            stubs.commentMock.expects('renderPage').withArgs(0);
-            annotator.showHighlightsOnPage(0);
-        });
-    });
-
     describe('removeRangyHighlight()', () => {
         it('should do nothing if there is not an array of highlights', () => {
             annotator.highlighter = {
@@ -1560,7 +1483,6 @@ describe('doc/DocAnnotator', () => {
         beforeEach(() => {
             const selection = document.getSelection();
             stubs.removeSelection = sandbox.stub(selection, 'removeAllRanges');
-            sandbox.stub(annotator, 'showHighlightsOnPage');
             sandbox.stub(annotator, 'toggleAnnotationMode');
             annotator.createHighlightDialog = {
                 isVisible: true,
@@ -1586,11 +1508,6 @@ describe('doc/DocAnnotator', () => {
             annotator.createHighlightDialog = { isVisible: false };
             annotator.handleControllerEvents({ event: CONTROLLER_EVENT.toggleMode, mode });
             expect(stubs.removeSelection).to.not.be.called;
-        });
-
-        it('should show highlights on page on showhighlights', () => {
-            annotator.handleControllerEvents({ event: CONTROLLER_EVENT.showHighlights, data: 1 });
-            expect(annotator.showHighlightsOnPage).to.be.calledWith(1);
         });
 
         it('should hide the createHighlightDialog on binddomlisteners', () => {