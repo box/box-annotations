b50cacc06507f9da1da45a7ebf485cc4fff9e261
Sumedha Pramod <spramod@box.com>
2018-03-15 18:53:39 -0700

    Chore: Don't allow selection of saved thread in draw mode

--- src/controllers/DrawingModeController.js
+++ src/controllers/DrawingModeController.js
@@ -217,7 +217,8 @@ class DrawingModeController extends AnnotationModeController {
      */
     handleSelection(event) {
         // NOTE: @jpress This is a workaround when buttons are not given precedence in the event chain
-        if (!event || (event.target && event.target.nodeName === 'BUTTON')) {
+        const hasPendingDrawing = this.currentThread && this.currentThread.state === STATES.pending;
+        if (!event || (event.target && event.target.nodeName === 'BUTTON') || hasPendingDrawing) {
             return;
         }
 
