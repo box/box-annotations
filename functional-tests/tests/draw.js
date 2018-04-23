90af90986c9f9cbe75bc78078e71eaaaaaa54b53
Sumedha Pramod <pramods@umich.edu>
2018-04-23 16:40:26 -0400

    Chore: Add tests to select/delete drawing (#180)

--- functional-tests/tests/draw.js
+++ functional-tests/tests/draw.js
@@ -1,4 +1,5 @@
 const {
+    SELECTOR_TEXT_LAYER,
     SELECTOR_DISABLED,
     SELECTOR_ANNOTATIONS_LOADED,
     SELECTOR_ANNNOTATION_MODE_BACKGROUND,
@@ -10,10 +11,11 @@ const {
     SELECTOR_ANNOTATION_LAYER_DRAW_IN_PROGRESS,
     SELECTOR_ANNOTATION_DRAWING_DIALOG,
     SELECTOR_ADD_DRAWING_BTN,
+    SELECTOR_ANNOTATION_DRAWING_LABEL,
     SELECTOR_DELETE_DRAWING_BTN
 } = require('../helpers/constants');
 
-const { draw } = require('../helpers/mouseEvents');
+const { draw, clickAtLocation } = require('../helpers/mouseEvents');
 
 Feature('Draw Annotation Sanity');
 
@@ -48,9 +50,9 @@ Scenario('Cancel a new drawing annotation @desktop', (I) => {
 
     I.say('Enter draw annotation mode');
     I.click(SELECTOR_ANNOTATION_BUTTON_DRAW);
-    I.click('.textLayer');
+    I.click(SELECTOR_TEXT_LAYER);
 
-    draw(I, '.textLayer');
+    draw(I, SELECTOR_TEXT_LAYER);
     I.waitForVisible(SELECTOR_ANNOTATION_LAYER_DRAW_IN_PROGRESS);
     I.waitForVisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
 
@@ -63,23 +65,58 @@ Scenario('Cancel a new drawing annotation @desktop', (I) => {
     I.waitForInvisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
 });
 
-Scenario('Create a drawing annotation @desktop', (I) => {
+Scenario('Create/Delete a drawing annotation w/ drawing dialog @desktop', (I) => {
     I.waitForVisible(SELECTOR_ANNOTATIONS_LOADED);
     I.waitForVisible(SELECTOR_ANNOTATION_BUTTON_DRAW);
 
     I.say('Enter draw annotation mode');
     I.click(SELECTOR_ANNOTATION_BUTTON_DRAW);
-    I.click('.textLayer');
+    I.click(SELECTOR_TEXT_LAYER);
 
-    draw(I, '.textLayer');
+    draw(I, SELECTOR_TEXT_LAYER, 100);
     I.waitForVisible(SELECTOR_ANNOTATION_LAYER_DRAW_IN_PROGRESS);
     I.waitForVisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
 
-    I.say('Undo/redo buttons should be disabled');
+    I.say('Undo/redo buttons should be appropriately disabled');
     I.waitForEnabled(SELECTOR_ANNOTATION_BUTTON_DRAW_UNDO);
     I.waitForVisible(`${SELECTOR_ANNOTATION_BUTTON_DRAW_REDO}${SELECTOR_DISABLED}`);
+    I.click(SELECTOR_ANNOTATION_BUTTON_DRAW_UNDO);
+    I.waitForVisible(`${SELECTOR_ANNOTATION_BUTTON_DRAW_UNDO}${SELECTOR_DISABLED}`);
+    I.waitForEnabled(SELECTOR_ANNOTATION_BUTTON_DRAW_REDO);
+    I.click(SELECTOR_ANNOTATION_BUTTON_DRAW_REDO);
 
     I.say('Save drawing');
     I.click(SELECTOR_ADD_DRAWING_BTN);
+    I.waitForInvisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
+    I.click(SELECTOR_ANNOTATION_BUTTON_DRAW_POST);
+
+    I.say('Select drawing')
+    clickAtLocation(I, SELECTOR_TEXT_LAYER, 300);
+    I.scrollTo(SELECTOR_ANNOTATION_DRAWING_DIALOG);
+    I.waitForVisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
+
+    I.say('Drawing should have a boundary and dialog should appear');
+    I.waitForText('Kanye West drew', 9, SELECTOR_ANNOTATION_DRAWING_LABEL);
+    I.waitForEnabled(SELECTOR_DELETE_DRAWING_BTN);
+
+    I.say('Delete drawing');
+    I.click(SELECTOR_DELETE_DRAWING_BTN);
+    I.waitForInvisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
+});
+
+Scenario('Create a drawing annotation by exiting mode @desktop', (I) => {
+    I.waitForVisible(SELECTOR_ANNOTATIONS_LOADED);
+    I.waitForVisible(SELECTOR_ANNOTATION_BUTTON_DRAW);
+
+    I.say('Enter draw annotation mode');
+    I.click(SELECTOR_ANNOTATION_BUTTON_DRAW);
+    I.click(SELECTOR_TEXT_LAYER);
+
+    draw(I, SELECTOR_TEXT_LAYER, 50);
+    I.waitForVisible(SELECTOR_ANNOTATION_LAYER_DRAW_IN_PROGRESS);
+    I.waitForVisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
+
+    I.say('Save drawing');
+    I.click(SELECTOR_ANNOTATION_BUTTON_DRAW_POST);
     I.waitForInvisible(SELECTOR_ANNOTATION_DRAWING_DIALOG);
 });
