4b3a63e9aeab364d75713fd729a583770f650abf
Sumedha Pramod <spramod@box.com>
2018-08-28 17:45:33 -0700

    Chore: Separating elements and annotations messages

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /**
  * @flow
- * @file i18n messages
+ * @file i18n messages from Box UI Elements
  * @author Box
  */
 
@@ -10,124 +10,74 @@ import type { MessageDescriptor } from 'react-intl';
 
 const messages: { [string]: MessageDescriptor } = defineMessages({
     commentCancel: {
-        id: 'ba.commentCancel',
+        id: 'be.commentCancel',
         defaultMessage: 'Cancel',
         description: 'Text for cancel button'
     },
     commentDeleteCancel: {
-        id: 'ba.commentDeleteCancel',
+        id: 'be.commentDeleteCancel',
         defaultMessage: 'No',
         description: 'Button text to cancel annotation deletion'
     },
     commentDeleteConfirm: {
-        id: 'ba.commentDeleteConfirm',
+        id: 'be.commentDeleteConfirm',
         defaultMessage: 'Yes',
         description: 'Button text to confirm annotation deletion'
     },
     commentDeletePrompt: {
-        id: 'ba.commentDeletePrompt',
+        id: 'be.commentDeletePrompt',
         defaultMessage: 'Delete annotation?',
         description: 'Confirmation prompt text to delete annotation'
     },
     commentPost: {
-        id: 'ba.commentPost',
+        id: 'be.commentPost',
         defaultMessage: 'Post',
         description: 'Text for post button'
     },
     commentShowOriginal: {
-        id: 'ba.commentShowOriginal',
+        id: 'be.commentShowOriginal',
         defaultMessage: 'Show Original',
         description: 'Show original button for showing original annotation'
     },
     commentTranslate: {
-        id: 'ba.commentTranslate',
+        id: 'be.commentTranslate',
         defaultMessage: 'Translate',
         description: 'Translate button for translating annotation'
     },
     commentWrite: {
-        id: 'ba.commentWrite',
+        id: 'be.commentWrite',
         defaultMessage: 'Add an annotation',
         description: 'Placeholder for annotation input'
     },
     commentPostedFullDateTime: {
-        id: 'ba.commentPostedFullDateTime',
+        id: 'be.commentPostedFullDateTime',
         defaultMessage: '{time, date, full} at {time, time, short}',
         description: 'Comment posted full date time for title'
     },
     commentCreateErrorMessage: {
-        id: 'ba.commentCreateErrorMessage',
+        id: 'be.commentCreateErrorMessage',
         description: 'Error message when an annotation creation fails',
         defaultMessage: 'There was an error creating this annotation.'
     },
     commentCreateConflictMessage: {
-        id: 'ba.commentCreateConflictMessage',
+        id: 'be.commentCreateConflictMessage',
         description: 'Error message when an annotation creation fails due to a conflict',
         defaultMessage: 'This annotation already exists.'
     },
     commentDeleteErrorMessage: {
-        id: 'ba.commentDeleteErrorMessage',
+        id: 'be.commentDeleteErrorMessage',
         description: 'Error message when an annotation deletion fails',
         defaultMessage: 'There was an error deleting this annotation.'
     },
     deleteLabel: {
-        id: 'ba.deleteLabel',
+        id: 'be.deleteLabel',
         defaultMessage: 'Delete',
         description: 'Aria label for button to delete a comment or task'
     },
     editLabel: {
-        id: 'ba.editLabel',
+        id: 'be.editLabel',
         defaultMessage: 'Edit',
         description: 'Aria label for button to edit a comment or task'
-    },
-    loadErrorMessage: {
-        id: 'ba.loadErrorMessage',
-        description: 'Error when annotations fail to load',
-        defaultMessage: 'We\'re sorry, annotations failed to load for this file.'
-    },
-    authErrorMessage: {
-        id: 'ba.authErrorMessage',
-        description: 'Error when the user\'s token has expired',
-        defaultMessage: 'Your session has expired. Please refresh the page.'
-    },
-    posting: {
-        id: 'ba.posting',
-        description: 'Placeholder when the annotation is pending',
-        defaultMessage: 'Posting...'
-    },
-    anonymousUserName: {
-        id: 'ba.anonymousUserName',
-        description: 'Placeholder when the current annotation\'s user information is unknown',
-        defaultMessage: 'Some User'
-    },
-    pointToggle: {
-        id: 'ba.pointToggle',
-        description: 'Point annotation mode toggle',
-        defaultMessage: 'Point annotation mode'
-    },
-    highlightToggle: {
-        id: 'ba.highlightToggle',
-        description: 'Plain highlight annotation toggle',
-        defaultMessage: 'Highlight tex'
-    },
-    highlightCommentToggle: {
-        id: 'ba.highlightComment',
-        description: 'Highlight comment annotation toggle',
-        defaultMessage: 'Add comment to highlighted text'
-    },
-    whoHighlighted: {
-        id: 'ba.whoHighlighted',
-        description: 'Label for who highlighted the annotated text',
-        defaultMessage: '{1} highlighted'
-    },
-    drawToggle: {
-        id: 'ba.drawToggle',
-        description: 'Drawing annotation mode toggle',
-        defaultMessage: 'Drawing annotation mode'
-    },
-    whoDrew: {
-        id: 'ba.whoDrew',
-        description: 'Label for who drew the drawing annotation',
-        defaultMessage: '{1} drew'
     }
 });
 
