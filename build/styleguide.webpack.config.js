bc1822e230735f9f959ebe83b9553e775da03cb5
Sumedha Pramod <pramods@umich.edu>
2018-08-29 10:01:16 -0700

    New: Setup styleguide component examples (#212)

---
+++ build/styleguide.webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    minetype: 'application/font-woff/',
+                    name: 'fonts/[name].[ext]'
+                }
+            },
+            {
+                test: /\.png$/,
+                loader: 'url-loader'
+            },
+            {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            // Map to uncompiled source code so we get nice source maps for debugging
+            'box-annotations/lib': path.join(__dirname, '../src'),
+            Wrapper: path.join(__dirname, '../src/components/Wrapper')
+        }
+    }
+};
