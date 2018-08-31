a7d15823eae806d8c14b6237f87d7f687c574a75
Sumedha Pramod <spramod@box.com>
2018-08-31 15:56:57 -0700

    New: Add duplicated components into a third-party folder

---
+++ third-party/interactionTargets.js
@@ -0,0 +1,76 @@
+export const SIDEBAR_NAV_TARGETS = {
+    ACTIVITY: 'sidebaractivity',
+    DETAILS: 'sidebardetails',
+    SKILLS: 'sidebarskills',
+    METADATA: 'sidebarmetadata'
+};
+
+export const SECTION_TARGETS = {
+    FILE_PROPERTIES: 'fileproperties',
+    ACCESS_STATS: 'accessstats'
+};
+
+export const DETAILS_TARGETS = {
+    ACCESS_STATS: {
+        COMMENTS: 'accessstats-comments',
+        DOWNLOADS: 'accessstats-downloads',
+        EDITS: 'accessstats-edits',
+        PREVIEWS: 'accessstats-previews',
+        VIEWS: 'accessstats-views',
+        VIEW_DETAILS: 'accessstats-viewdetails'
+    },
+    CLASSIFICATION_ADD: 'addclassification',
+    CLASSIFICATION_EDIT: 'editclassification',
+    DESCRIPTION: 'description',
+    VERSION_HISTORY: 'versionhistory'
+};
+
+export const ACTIVITY_TARGETS = {
+    INLINE_EDIT: 'activityfeed-inlineedit',
+    INLINE_DELETE: 'activityfeed-inlinedelete',
+    APPROVAL_FORM_ADD_TASK: 'activityfeed-apporvalformaddtask',
+    APPROVAL_FORM_CANCEL: 'activityfeed-approvalformcancel',
+    APPROVAL_FORM_POST: 'activityfeed-approvalformpost',
+    PROFILE: 'activityfeed-profilelink',
+    MENTION: 'activityfeed-mentionlink',
+    VERSION_CARD: 'activityfeed-versioninfo',
+    TASK_DATE_PICKER: 'activityfeed-taskdatepicker',
+    TASK_APPROVE: 'activityfeed-taskapprove',
+    TASK_REJECT: 'activityfeed-taskreject'
+};
+
+export const SKILLS_TARGETS = {
+    FACES: {
+        CARD: 'skill-facecard',
+        FACE: 'skill-face',
+        DELETE: 'skill-facedelete',
+        EDIT: 'skill-faceedit',
+        EDIT_SAVE: 'skill-faceeditsave',
+        EDIT_CANCEL: 'skill-faceeditcancel',
+        TIMELINE: 'skill-facetimeslice'
+    },
+    KEYWORDS: {
+        CARD: 'skill-keywordcard',
+        EDIT: 'skill-keywordedit',
+        EDIT_SAVE: 'skill-keywordeditsave',
+        EDIT_CANCEL: 'skill-keywordeditcancel',
+        TIMELINE: 'skill-keywordtimeslice',
+        SELECT: 'skill-keywordselect'
+    },
+    TRANSCRIPTS: {
+        CARD: 'skill-transcriptcard',
+        COPY: 'skill-transcriptcopy',
+        EDIT: 'skill-transcriptedit',
+        EXPAND: 'skill-transcriptexpand',
+        EDIT_SAVE: 'skill-transcripteditsave',
+        EDIT_CANCEL: 'skill-transcripteditcancel',
+        TRANSCRIPT: 'skill-transcript',
+        EDIT_TEXT: 'skill-transcriptedittext'
+    },
+    TIMELINE: {
+        NEXT: 'skill-timelimelinenext',
+        PREVIOUS: 'skill-timelineprevious'
+    }
+};
+
+export const INTERACTION_TARGET = 'data-resin-target';
