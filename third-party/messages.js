697fa6e547c855a877d2007a136e65a436efe15b
Sumedha Pramod <spramod@box.com>
2018-08-31 16:28:26 -0700

    Chore: Removing unused messages

--- third-party/messages.js
+++ third-party/messages.js
@@ -9,11 +9,6 @@ import { defineMessages } from 'react-intl';
 import type { MessageDescriptor } from 'react-intl';
 
 const messages: { [string]: MessageDescriptor } = defineMessages({
-    commentCancel: {
-        id: 'be.commentCancel',
-        defaultMessage: 'Cancel',
-        description: 'Text for cancel button'
-    },
     commentDeleteCancel: {
         id: 'be.commentDeleteCancel',
         defaultMessage: 'No',
@@ -23,11 +18,6 @@ const messages: { [string]: MessageDescriptor } = defineMessages({
         id: 'be.commentDeleteConfirm',
         defaultMessage: 'Yes',
         description: 'Button text to confirm comment deletion'
-    },
-    commentPost: {
-        id: 'be.commentPost',
-        defaultMessage: 'Post',
-        description: 'Text for post button'
     },
     commentShowOriginal: {
         id: 'be.commentShowOriginal',
@@ -39,36 +29,11 @@ const messages: { [string]: MessageDescriptor } = defineMessages({
         defaultMessage: 'Translate',
         description: 'Translate button for translating comment'
     },
-    commentWrite: {
-        id: 'be.commentWrite',
-        defaultMessage: 'Add a comment',
-        description: 'Placeholder for comment input'
-    },
-    commentCreateErrorMessage: {
-        id: 'be.commentCreateErrorMessage',
-        description: 'Error message when a comment creation fails',
-        defaultMessage: 'There was an error creating this comment.'
-    },
-    commentCreateConflictMessage: {
-        id: 'be.commentCreateConflictMessage',
-        description: 'Error message when a comment creation fails due to a conflict',
-        defaultMessage: 'This comment already exists.'
-    },
-    commentDeleteErrorMessage: {
-        id: 'be.commentDeleteErrorMessage',
-        description: 'Error message when a comment deletion fails',
-        defaultMessage: 'There was an error deleting this comment.'
-    },
     deleteLabel: {
         id: 'be.deleteLabel',
         defaultMessage: 'Delete',
         description: 'Aria label for button to delete a comment or task'
     },
-    editLabel: {
-        id: 'be.editLabel',
-        defaultMessage: 'Edit',
-        description: 'Aria label for button to edit a comment or task'
-    }
 });
 
 export default messages;
