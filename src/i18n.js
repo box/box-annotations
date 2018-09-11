f084516c2af9da83f29bfef30fac6bfa4b2b1376
Sumedha Pramod <pramods@umich.edu>
2018-09-10 17:43:05 -0700

    Chore: Add box-locales (#216)

---
+++ src/i18n.js
@@ -0,0 +1,16 @@
+/**
+ * @flow
+ * @file Exports language, messages and react-intl language data for internationalization
+ * @author Box
+ */
+
+import localeData from 'react-intl-locale-data'; // eslint-disable-line
+import annotationsLocaleData from 'box-annotations-locale-data'; // eslint-disable-line
+import boxReactUILocaleData from 'box-react-ui-locale-data'; // eslint-disable-line
+
+declare var __LANGUAGE__: string;
+
+const language = __LANGUAGE__;
+const messages = Object.assign({}, annotationsLocaleData, boxReactUILocaleData);
+
+export default { language, messages, localeData };
