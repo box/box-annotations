fba71ef4f7a095eb85320bd24b890fc0028aa999
Sumedha Pramod <spramod@box.com>
2018-09-18 16:19:46 -0700

    New: AnnotatorLabel component

--- src/components/ActionControls/ActionControls.js
+++ src/components/ActionControls/ActionControls.js
@@ -7,15 +7,13 @@ import PlainButton from 'box-react-ui/lib/components/plain-button';
 import IconHighlightAnnotation from 'box-react-ui/lib/icons/annotations/IconHighlightAnnotation';
 import IconHighlightCommentAnnotation from 'box-react-ui/lib/icons/annotations/IconHighlightCommentAnnotation';
 import IconClose from 'box-react-ui/lib/icons/general/IconClose';
-import CommentText from '../../../third-party/components/Comment/CommentText';
 
 import { TYPES } from '../../constants';
 import { isHighlightAnnotation } from '../../util';
 import Internationalize from '../Internationalize';
 
-import messages from './messages';
-
 import './ActionControls.scss';
+import AnnotatorLabel from './AnnotatorLabel';
 
 type Props = {
     id: string,
@@ -31,68 +29,45 @@ type Props = {
     intl: any
 };
 
-class ActionControls extends React.PureComponent<Props> {
-    getAnnotationLabel() {
-        const { type, currentUser, intl } = this.props;
-        const anonymousUserName = intl.formatMessage(messages.anonymousUserName);
-        const annotatorName = !!currentUser && !!currentUser.name ? currentUser.name : anonymousUserName;
+const ActionControls = ({
+    id,
+    type,
+    itemPermissions,
+    permissions,
+    onCreate,
+    onCommentClick,
+    isPending,
+    currentUser,
+    language,
+    messages: intlMessages
+}: Props) => {
+    const isHighlight = isHighlightAnnotation(type);
+    const isDrawing = type === TYPES.draw;
+    const canComment = isHighlight && getProp(itemPermissions, 'can_annotate', false);
+    const canDelete = getProp(permissions, 'can_delete', false);
 
-        if (isHighlightAnnotation(type)) {
-            return intl.formatMessage(messages.whoHighlighted, { name: annotatorName });
-        }
-
-        if (type === TYPES.draw) {
-            return intl.formatMessage(messages.whoDrew, { name: annotatorName });
-        }
-
-        return intl.formatMessage(messages.whoAnnotated, { name: annotatorName });
-    }
-
-    render() {
-        const {
-            id,
-            type,
-            itemPermissions,
-            permissions,
-            onCreate,
-            onCommentClick,
-            isPending,
-            language,
-            messages: intlMessages
-        } = this.props;
-        const isHighlight = isHighlightAnnotation(type);
-        const isDrawing = type === TYPES.draw;
-        const canComment = !!(isHighlight && getProp(itemPermissions, 'can_annotate', false));
-        const canDelete = getProp(permissions, 'can_delete', false);
-        const annotatorLabelMessage = this.getAnnotationLabel();
-
-        return (
-            <Internationalize language={language} messages={intlMessages}>
-                <div className='ba-action-controls'>
-                    {!isPending && (
-                        <span className='ba-annotation-label'>
-                            <CommentText id={id} tagged_message={annotatorLabelMessage} translationEnabled={false} />
-                        </span>
-                    )}
-                    {!!(isHighlight && canDelete) && (
-                        <PlainButton type='button' className='ba-highlight-btn' onClick={onCreate}>
-                            <IconHighlightAnnotation />
-                        </PlainButton>
-                    )}
-                    {canComment && (
-                        <PlainButton type='button' className='ba-highlight-comment-btn' onClick={onCommentClick}>
-                            <IconHighlightCommentAnnotation />
-                        </PlainButton>
-                    )}
-                    {!!(isDrawing && canDelete) && (
-                        <PlainButton type='button' className='ba-drawing-delete-btn' onClick={onCommentClick}>
-                            <IconClose />
-                        </PlainButton>
-                    )}
-                </div>
-            </Internationalize>
-        );
-    }
-}
+    return (
+        <Internationalize language={language} messages={intlMessages}>
+            <div className='ba-action-controls'>
+                {!isPending && <AnnotatorLabel id={id} type={type} currentUser={currentUser} />}
+                {!!(isHighlight && canDelete) && (
+                    <PlainButton type='button' className='ba-highlight-btn' onClick={onCreate}>
+                        <IconHighlightAnnotation />
+                    </PlainButton>
+                )}
+                {canComment && (
+                    <PlainButton type='button' className='ba-highlight-comment-btn' onClick={onCommentClick}>
+                        <IconHighlightCommentAnnotation />
+                    </PlainButton>
+                )}
+                {!!(isDrawing && canDelete) && (
+                    <PlainButton type='button' className='ba-drawing-delete-btn' onClick={onCommentClick}>
+                        <IconClose />
+                    </PlainButton>
+                )}
+            </div>
+        </Internationalize>
+    );
+};
 
 export default injectIntl(ActionControls);
