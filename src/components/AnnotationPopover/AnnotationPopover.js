408f7e2b5dda75f18f5a8a59dcf0740592daed0c
Sumedha Pramod <spramod@box.com>
2018-09-19 11:29:22 -0700

    Chore: Use ACF instead of AnnotationForm wrapper

--- src/components/AnnotationPopover/AnnotationPopover.js
+++ src/components/AnnotationPopover/AnnotationPopover.js
@@ -1,10 +1,11 @@
 // @flow
 import React from 'react';
+import noop from 'lodash/noop';
 import Overlay from 'box-react-ui/lib/components/flyout/Overlay';
+import ApprovalCommentForm from '../../../third-party/components/ApprovalCommentForm';
 
 import Internationalize from '../Internationalize';
 import AnnotationList from '../AnnotationList';
-import AnnotationForm from '../AnnotationForm';
 
 import './AnnotationPopover.scss';
 
@@ -19,10 +20,29 @@ type Props = {
     messages?: StringMap
 };
 
-class AnnotationPopover extends React.Component<Props> {
+type State = {
+    isInputOpen: boolean
+};
+
+const NULL_USER = {};
+const IS_EDITING_TRUE = true;
+
+class AnnotationPopover extends React.Component<Props, State> {
     defaultProps = {
         isPending: false
     };
+
+    state = {
+        isInputOpen: true
+    };
+
+    componentWillMount() {
+        this.setState({ isInputOpen: true });
+    }
+
+    componentWillUnmount() {
+        this.setState({ isInputOpen: false });
+    }
 
     hasAnnotations = () => {
         const { annotations } = this.props;
@@ -31,11 +51,24 @@ class AnnotationPopover extends React.Component<Props> {
 
     render() {
         const { annotations, canAnnotate, onDelete, onCancel, onCreate, language, messages: intlMessages } = this.props;
+        const { isInputOpen } = this.state;
         return (
             <Internationalize language={language} messages={intlMessages}>
                 <Overlay className='ba-annotation-popover'>
                     {this.hasAnnotations() && <AnnotationList annotations={annotations} onDelete={onDelete} />}
-                    {canAnnotate && <AnnotationForm onCreate={onCreate} onCancel={onCancel} />}
+                    {canAnnotate && (
+                        <ApprovalCommentForm
+                            className='ba-annotation-input-container'
+                            user={NULL_USER}
+                            isOpen={isInputOpen}
+                            isEditing={IS_EDITING_TRUE}
+                            createComment={onCreate}
+                            onCancel={onCancel}
+                            onSubmit={noop}
+                            onFocus={noop}
+                            getAvatarUrl={noop}
+                        />
+                    )}
                 </Overlay>
             </Internationalize>
         );
