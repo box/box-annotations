59195036d734041024efd2cc964f550a473674b3
Sumedha Pramod <spramod@box.com>
2018-10-10 16:05:09 -0700

    Chore: Cleanup components

--- src/components/AnnotationForm/README.md
+++
@@ -1,9 +0,0 @@
-### Description
-Component to represent the wrapper around the ApprovalCommentForm component
-
-### Examples
-```jsx
-const AnnotationFormExample = require('examples').AnnotationFormExample;
-
-<AnnotationFormExample />
-```
--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -3,27 +3,21 @@ import React from 'react';
 
 import Comment from '../Comment';
 
-import Internationalize from '../Internationalize';
-
 import './CommentList.scss';
 
 type Props = {
     comments: Comments,
-    language?: string,
-    messages?: StringMap,
     onDelete: Function
 };
 
-const CommentList = ({ comments, language, messages: intlMessages, onDelete }: Props) => (
-    <Internationalize language={language} messages={intlMessages}>
-        <ul className='ba-comment-list'>
-            {comments.map(({ id, ...rest }) => (
-                <li className='ba-comment-list-item' key={`annotation_${id}`}>
-                    <Comment id={id} onDelete={onDelete} {...rest} />
-                </li>
-            ))}
-        </ul>
-    </Internationalize>
+const CommentList = ({ comments, onDelete }: Props) => (
+    <ul className='ba-comment-list'>
+        {comments.map(({ id, ...rest }) => (
+            <li className='ba-comment-list-item' key={`annotation_${id}`}>
+                <Comment id={id} onDelete={onDelete} {...rest} />
+            </li>
+        ))}
+    </ul>
 );
 
 export default CommentList;
