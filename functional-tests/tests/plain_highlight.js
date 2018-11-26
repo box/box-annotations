2007d711ffba6159b37408ea83cce93deac60a3b
Sumedha Pramod <spramod@box.com>
2018-11-26 13:55:22 -0800

    Fix: Functional tests to work with React components

--- functional-tests/tests/plain_highlight.js
+++ functional-tests/tests/plain_highlight.js
@@ -1,12 +1,11 @@
 /* eslint-disable prefer-arrow-callback, no-var, func-names */
 const {
-    SELECTOR_ACTIVE,
     SELECTOR_TEXT_LAYER,
     SELECTOR_ANNOTATIONS_LOADED,
-    SELECTOR_ANNOTATION_HIGHLIGHT_DIALOG,
-    SELECTOR_ANNOTATION_DIALOG,
-    SELECTOR_ADD_HIGHLIGHT_BTN,
-    SELECTOR_HIGHLIGHT_LABEL
+    SELECTOR_HIGHLIGHT_CONTROLS,
+    SELECTOR_ANNOTATION_POPOVER,
+    SELECTOR_HIGHLIGHT_BTN,
+    SELECTOR_ANNOTATOR_LABEL
 } = require('../helpers/constants');
 
 const { selectText } = require('../helpers/mouseEvents');
@@ -31,24 +30,24 @@ Scenario('Create/Delete a new plain highlight annotation @desktop @doc', functio
      */
     I.say('Highlight dialog should appear after selecting text');
     selectText(I, SELECTOR_TEXT_LAYER);
-    I.waitForVisible(SELECTOR_ANNOTATION_HIGHLIGHT_DIALOG);
+    I.waitForVisible(SELECTOR_HIGHLIGHT_CONTROLS);
 
     I.say('Highlight selected text');
-    I.click(SELECTOR_ADD_HIGHLIGHT_BTN);
+    I.click(SELECTOR_HIGHLIGHT_BTN);
 
     I.say('Highlight should be created and dialog should appear');
-    I.waitForVisible(SELECTOR_ANNOTATION_DIALOG);
-    I.waitForText('Kanye West highlighted', 9, SELECTOR_HIGHLIGHT_LABEL);
-    I.waitForEnabled(SELECTOR_ADD_HIGHLIGHT_BTN);
+    I.waitForVisible(SELECTOR_HIGHLIGHT_CONTROLS);
+    I.waitForText('Kanye West highlighted', 9, SELECTOR_ANNOTATOR_LABEL);
+    I.waitForEnabled(SELECTOR_HIGHLIGHT_BTN);
 
-    validateIconColor(I, `${SELECTOR_ADD_HIGHLIGHT_BTN}${SELECTOR_ACTIVE}`, 'rgb(255, 201, 0)');
+    validateIconColor(I, `${SELECTOR_HIGHLIGHT_BTN}`, 'rgb(255, 201, 0)');
 
     /*
      * Delete plain highlight annotation
      */
     I.say('Delete the highlight annotation');
-    I.click(`${SELECTOR_ADD_HIGHLIGHT_BTN}${SELECTOR_ACTIVE}`);
+    I.click(`${SELECTOR_HIGHLIGHT_BTN}`);
 
     I.say('Highlight should be deleted');
-    I.waitForDetached(SELECTOR_ANNOTATION_DIALOG, 1);
+    I.waitForDetached(SELECTOR_ANNOTATION_POPOVER, 1);
 });
