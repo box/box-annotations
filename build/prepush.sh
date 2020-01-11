3157b69383846d8ad7c78310cef98a283ea3e517
Jared Stoffan <jstoffan@box.com>
2020-01-10 18:33:06 -0800

    build(hooks): Remove unnecessary steps from prepush script

--- build/prepush.sh
+++ build/prepush.sh
@@ -21,24 +21,20 @@ check_and_commit_updated_translations() {
 # lint, test, and build assets to update translations
 prepush() {
     echo "--------------------------------------------------------"
-    echo "Linting"
-    echo "--------------------------------------------------------"
-    yarn lint || exit 1
-
-    echo "--------------------------------------------------------"
-    echo "Checking flow types"
+    echo "Checking types"
     echo "--------------------------------------------------------"
     yarn flow check || exit 1
-
-    echo "--------------------------------------------------------"
-    echo "Testing"
-    echo "--------------------------------------------------------"
-    yarn test || exit 1
 
     echo "--------------------------------------------------------"
     echo "Building"
     echo "--------------------------------------------------------"
     yarn build || exit 1
+
+    echo "--------------------------------------------------------"
+    echo "Testing"
+    echo "--------------------------------------------------------"
+    git fetch upstream
+    yarn test --changedSince=upstream/master || exit 1
 
     check_and_commit_updated_translations
 }
