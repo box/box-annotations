f3b490c1cab85130d69d59b0c5d9732c6c5d3b32
Mingze <mxiao@box.com>
2020-04-20 17:57:35 -0700

    feat(popups): Add ReplyField component (#444)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/components/Popups/ReplyField/ReplyField.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import classnames from 'classnames';
+
+export type Props = {
+    className?: string;
+    defaultValue?: string;
+    onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    onClick: (event: React.SyntheticEvent) => void;
+};
+
+const ReplyField = (props: Props, ref: React.Ref<HTMLTextAreaElement>): JSX.Element => {
+    const { className, ...rest } = props;
+
+    return <textarea ref={ref} className={classnames('ba-TextArea', className)} {...rest} />;
+};
+
+export default React.forwardRef(ReplyField);
