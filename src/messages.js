16c78eb4b245ec32b7d84679677e9cc62c962a4c
Jared Stoffan <jstoffan@users.noreply.github.com>
2020-04-21 13:39:34 -0700

    chore(cleanup): Remove unused i18n messages and fix prepush (#451)

--- src/messages.js
+++ src/messages.js
@@ -1,11 +1,6 @@
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
-    anonymousUser: {
-        id: 'ba.anonymousUser',
-        description: 'Place holder for the anonymous user',
-        defaultMessage: 'Some User',
-    },
     annotationsClose: {
         id: 'ba.annotationsClose',
         description: 'Label for the close button',
@@ -21,29 +16,14 @@ export default defineMessages({
         description: 'Label for the post button',
         defaultMessage: 'Post',
     },
-    annotationHighlightToggle: {
-        id: 'ba.annotationHighlightToggle',
-        description: 'Accessibility message for button that toggles highlight annotation mode ',
-        defaultMessage: 'Highlight text',
-    },
-    annotationsAuthError: {
-        id: 'ba.annotationsAuthError',
-        description: 'Error message when Authorizing',
-        defaultMessage: 'Your session has expired. Please refresh the page',
-    },
-    annotationsLoadError: {
-        id: 'ba.annotationsLoadError',
-        description: 'Error message when loading',
-        defaultMessage: 'We’re sorry, annotations failed to load for this file',
-    },
     annotationsCreateError: {
         id: 'ba.annotationsCreateError',
         description: 'Error message when creating',
         defaultMessage: 'We’re sorry, the annotation could not be created.',
     },
-    annotationsDeleteError: {
-        id: 'ba.annotationsDeleteError',
-        description: 'Error message when deleting an annotation',
-        defaultMessage: 'We’re sorry, the annotation could not be deleted.',
+    annotationsLoadError: {
+        id: 'ba.annotationsLoadError',
+        description: 'Error message when loading',
+        defaultMessage: 'We’re sorry, the annotations failed to load for this file.',
     },
 });
