bbc10935547d91e85792dd8b924c578504759ed8
Jared Stoffan <jstoffan@box.com>
2020-04-21 17:06:06 -0700

    chore(jest): Fix yarn registry entries and jest coverage errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,10 @@ module.exports = {
     rules: {
         camelcase: 0, // fixme
         'class-methods-use-this': 0, // fixme
+        'flowtype/no-types-missing-file-annotation': 'off', // Allows types in TS files
         'import/no-extraneous-dependencies': ['error', { devDependencies: ['scripts/**/*.js', '**/*-test.[j|t]s*'] }],
         'import/no-unresolved': 'off', // Allows JS files to import TS files
         'prefer-destructuring': ['error', { object: true, array: false }],
         'react/default-props-match-prop-types': 0,
     },
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            rules: {
-                'flowtype/no-types-missing-file-annotation': 'off',
-            },
-        },
-    ],
 };
