52926ecd9df207c4e150e56c680213db885a5c1c
Jared Stoffan <jstoffan@box.com>
2020-07-24 11:54:08 -0700

    test(cypress): Update test script to better handle edge cases

--- scripts/cypress.js
+++ scripts/cypress.js
@@ -1,10 +1,31 @@
 /* eslint-disable no-console */
-
 const BoxSDK = require('box-node-sdk');
 const childProcess = require('child_process');
 
-const { env } = process;
-const { E2E_ACCESS_TOKEN, E2E_CLIENT_ID, E2E_PARENT_FOLDER_ID } = env;
+const { argv, env } = process;
+const { E2E_ACCESS_TOKEN, E2E_CLIENT_ID } = env;
+const KILL_SIGNALS = [
+    'SIGABRT',
+    'SIGBUS',
+    'SIGFPE',
+    'SIGHUP',
+    'SIGILL',
+    'SIGINT',
+    'SIGQUIT',
+    'SIGSEGV',
+    'SIGTERM',
+    'SIGTRAP',
+    'SIGUSR1',
+    'SIGUSR2',
+];
+
+if (!E2E_ACCESS_TOKEN) {
+    throw new Error('E2E_ACCESS_TOKEN must be set as an environment variable');
+}
+
+if (!E2E_CLIENT_ID) {
+    throw new Error('E2E_CLIENT_ID must be set as an environment variable');
+}
 
 const sdk = new BoxSDK({
     clientID: E2E_CLIENT_ID,
@@ -13,19 +34,35 @@ const sdk = new BoxSDK({
 
 const client = sdk.getBasicClient(E2E_ACCESS_TOKEN);
 
+async function cleanup(folderId) {
+    console.log('Cleanup test folder and files...');
+
+    try {
+        await client.folders.delete(folderId, { recursive: true });
+        console.log('Cleanup complete.');
+    } catch (error) {
+        console.error(`Cleanup failed. Error: ${error.message}`);
+    }
+}
+
 async function main() {
     const testFolderName = `Test ${new Date().toISOString()}`;
 
+    // Bootstrap the test folder and copy template files into it
     console.log(`Setup test folder: ${testFolderName}...`);
-    const { id: folderId } = await client.folders.create(E2E_PARENT_FOLDER_ID, testFolderName);
+    const { id: folderId } = await client.folders.create('118537970832', testFolderName); // Test folder
     const { id: documentId } = await client.files.copy('694470903390', folderId); // Document template
     const { id: imageId } = await client.files.copy('694468799644', folderId); // Image template
     console.log('Setup complete.');
 
+    // Attempt to cleanup test folder before script is killed (example: CTL+C)
+    KILL_SIGNALS.forEach(signal => process.on(signal, () => cleanup(folderId)));
+
     try {
         console.log('Cypress run starting...');
 
-        const output = childProcess.execSync(`yarn test:e2e`, {
+        const suffix = argv.indexOf('-o') >= 0 ? 'open' : 'run'; // Pass -o to run Cypress in "open" mode
+        const output = childProcess.execSync(`yarn npm-run-all -p -r start:dev cy:${suffix}`, {
             env: {
                 ...env,
                 CYPRESS_ACCESS_TOKEN: E2E_ACCESS_TOKEN,
@@ -44,13 +81,7 @@ async function main() {
         process.exitCode = error && error.status ? error.status : 0;
     }
 
-    try {
-        console.log('Cleanup test folder and files...');
-        await client.folders.delete(folderId, { recursive: true });
-        console.log('Cleanup complete.');
-    } catch (error) {
-        console.error(`Cleanup failed. Error: ${error.message}`);
-    }
+    await cleanup(folderId);
 
     console.log('Test script complete. Exiting.');
 }
