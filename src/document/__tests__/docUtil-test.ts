0d7d60114f7a4d522dd4aa94b8f117e699f4b4f3
Mingze Xiao <mxiao@box.com>
2020-08-13 16:03:41 -0700

    feat(highlight): More tests

--- src/document/__tests__/docUtil-test.ts
+++ src/document/__tests__/docUtil-test.ts
@@ -56,9 +56,44 @@ describe('docUtil', () => {
     });
 
     describe('getSelectionItem()', () => {
+        const rootElement = document.createElement('div');
+        rootElement.innerHTML = `
+            <div class="range0" />
+            <div class="page" data-page-number="1">
+                <div class="range1" />
+            </div>
+            <div class="page" data-page-number="2">
+                <div class="range2" />
+            </div>
+        `;
+
+        const generateSelection = (startClass: string, endClass: string): Selection => {
+            return ({
+                getRangeAt: () => {
+                    const range = document.createRange();
+                    range.setStart(rootElement.querySelector(startClass) as Node, 0);
+                    range.setEnd(rootElement.querySelector(endClass) as Node, 0);
+                    range.getBoundingClientRect = jest.fn().mockReturnValueOnce({});
+                    return range;
+                },
+            } as unknown) as Selection;
+        };
+
         test('should return null if range is null', () => {
             jest.spyOn(window, 'getSelection').mockImplementationOnce(() => null);
             expect(getSelectionItem()).toBe(null);
+        });
+
+        test.each`
+            startClass   | endClass     | result
+            ${'.range0'} | ${'.range0'} | ${null}
+            ${'.range1'} | ${'.range0'} | ${null}
+            ${'.range1'} | ${'.range2'} | ${null}
+            ${'.range1'} | ${'.range1'} | ${expect.objectContaining({ location: 1 })}
+        `('should return $result', ({ startClass, endClass, result }) => {
+            jest.spyOn(window, 'getSelection').mockReturnValueOnce(generateSelection(startClass, endClass));
+
+            expect(getSelectionItem()).toEqual(result);
         });
     });
 });
