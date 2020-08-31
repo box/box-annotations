d0e7cfcc4f093d0b443c3b398207796480189f0a
Mingze <mxiao@box.com>
2020-08-31 09:53:07 -0700

    test(itemlist): Fix ItemList test console warning (#569)

--- src/components/ItemList/__tests__/ItemList-test.tsx
+++ src/components/ItemList/__tests__/ItemList-test.tsx
@@ -5,7 +5,7 @@ import ItemList, { Props } from '../ItemList';
 import { Collaborator } from '../../../@types';
 
 jest.mock('scroll-into-view-if-needed', () => jest.fn());
-jest.mock('../ItemRow', () => 'div');
+jest.mock('../ItemRow');
 
 describe('ItemList', () => {
     const defaults: Props<Collaborator> = {
