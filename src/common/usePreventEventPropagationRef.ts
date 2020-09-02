e282f6ca29d63036cea76e21e539b7ddb6c933d2
Conrad Chan <conradchan@box.com>
2020-09-02 10:36:38 -0700

    chore: cleanup usePreventEventPropagationRef

--- src/common/usePreventEventPropagationRef.ts
+++ src/common/usePreventEventPropagationRef.ts
@@ -6,14 +6,28 @@ const handleEvent = (event: Event): void => {
 };
 
 export default function usePreventEventPropagationRef<T extends HTMLElement>(...args: string[]): (element: T) => void {
-    return React.useCallback(
-        (element: T | null) => {
-            if (element === null) {
-                return;
-            }
-
-            args.forEach(event => element.addEventListener(event, handleEvent));
+    const [element, setElement] = React.useState<T | null>(null);
+    const ref = React.useCallback(
+        (el: T | null) => {
+            setElement(el);
         },
-        [args],
+        [args], // eslint-disable-line react-hooks/exhaustive-deps
     );
+
+    const cleanup = (): void => {
+        if (element === null) {
+            return;
+        }
+
+        args.forEach(event => element.removeEventListener(event, handleEvent));
+    };
+
+    React.useEffect(() => {
+        if (element !== null) {
+            args.forEach(event => element.addEventListener(event, handleEvent));
+        }
+        return cleanup;
+    }, [element, args]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return ref;
 }
