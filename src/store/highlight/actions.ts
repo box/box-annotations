b892e4363208569e0db9c23b41d5b5d98450a0f5
Mingze Xiao <mxiao@box.com>
2020-09-04 10:27:42 -0700

    fix(range): Fix Range.getClientRects on IE/Edge

--- src/store/highlight/actions.ts
+++ src/store/highlight/actions.ts
@@ -13,6 +13,59 @@ type Payload = {
     payload: SelectionItem | null;
 };
 
+declare global {
+    interface Document {
+        createNodeIterator(
+            root: Node,
+            whatToShow?: number,
+            filter?: NodeFilter | Function,
+            entityReferenceExpansion?: boolean,
+        ): NodeIterator;
+    }
+}
+
+export const getClientRects = (range: Range): DOMRect[] => {
+    const iterator = document.createNodeIterator(
+        range.commonAncestorContainer,
+        NodeFilter.SHOW_ALL,
+        function acceptNode() {
+            return NodeFilter.FILTER_ACCEPT;
+        },
+        false,
+    );
+
+    const newRange = document.createRange();
+    const rects = [];
+    let currentNode = iterator.nextNode();
+    while (currentNode) {
+        if (rects.length === 0 && currentNode !== range.startContainer) {
+            currentNode = iterator.nextNode();
+            // eslint-disable-next-line no-continue
+            continue;
+        }
+
+        // Only highlight Text nodes
+        if (currentNode.nodeType === Node.TEXT_NODE) {
+            newRange.selectNodeContents(currentNode);
+            if (currentNode === range.startContainer) {
+                newRange.setStart(currentNode, range.startOffset);
+            }
+            if (currentNode === range.endContainer) {
+                newRange.setEnd(currentNode, range.endOffset);
+            }
+            rects.push(newRange.getBoundingClientRect());
+        }
+
+        if (currentNode === range.endContainer) {
+            break;
+        }
+
+        currentNode = iterator.nextNode();
+    }
+
+    return rects;
+};
+
 export const getShape = ({ height, left, top, width }: DOMRect): Shape => ({
     height,
     width,
@@ -31,11 +84,17 @@ export const setSelectionAction = createAction(
 
         const { containerRect, location, range } = arg;
 
+        let rects = Array.from(range.getClientRects());
+        // getClientRects on IE/Edge might return 0 rects
+        if (!rects.length) {
+            rects = getClientRects(range);
+        }
+
         return {
             payload: {
                 containerRect: getShape(containerRect),
                 location,
-                rects: combineRectsByRow(Array.from(range.getClientRects()).map(getShape)),
+                rects: combineRectsByRow(rects.map(getShape)),
             },
         };
     },
