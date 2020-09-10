2cdd9ce41c40c9a72c9f7019079e266e2a507052
Conrad Chan <conradchan@box.com>
2020-09-10 10:04:52 -0700

    chore(eventing): Add annotations_mode_change event back (#584)

---
+++ src/store/eventing/mode.ts
@@ -0,0 +1,7 @@
+import eventManager from '../../common/EventManager';
+import { AppState } from '../types';
+import { Event } from '../../@types';
+
+export const handleToggleAnnotationModeAction = (prevState: AppState, nextState: AppState): void => {
+    eventManager.emit(Event.ANNOTATIONS_MODE_CHANGE, { mode: nextState.common.mode });
+};
