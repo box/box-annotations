990dd6ba286365c8e9aa45c58379934bb5f8c075
Jack Chen <yuxi.chen@berkeley.edu>
2020-09-10 20:32:20 -0700

    feat(discoverability): update OptionState type

--- src/store/options/types.ts
+++ src/store/options/types.ts
@@ -4,6 +4,7 @@ export type OptionsState = {
     fileId: string | null;
     fileVersionId: string | null;
     isCurrentFileVersion: boolean;
+    isDiscoverabilityFeatureEnabled: boolean;
     permissions: Permissions;
     rotation: number;
     scale: number;
