0d00b10d958a836fcafe382dc81b07fdd322a307
Mingze <mxiao@box.com>
2020-10-09 16:46:08 -0700

    test(cypress): Add E2E tests for promoting highlight annotations (#623)
    * test(cypress): Add E2E tests for promoting highlight annotations
    
    * test(cypress): Address comments

--- test/integration/Highlight.e2e.test.js
+++ test/integration/Highlight.e2e.test.js
@@ -35,5 +35,21 @@ describe('Highlights', () => {
 
         // Assert that annotation target is active
         cy.get('.ba-HighlightTarget').should('have.class', 'is-active');
+
+        // Select text to trigger promotion flow
+        cy.selectText({ block: 2 });
+        cy.getByTestId('ba-PopupHighlight-button').click();
+        // Assert highlight creation mode is active
+        cy.getByTestId('bp-AnnotationsControls-highlightBtn').should('have.class', 'is-active');
+        // Create highlight annotation
+        cy.submitReply();
+
+        // Assert that one more highlight annotation is present on the document and is active
+        cy.get('.ba-HighlightTarget')
+            .should('have.length', 2)
+            .should('have.class', 'is-active');
+
+        // Assert highlight creation mode is not active
+        cy.getByTestId('bp-AnnotationsControls-highlightBtn').should('not.have.class', 'is-active');
     });
 });
