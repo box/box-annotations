0a30b17ea42af85714fadcfe6611e789970a9c0c
Mingze Xiao <mxiao@box.com>
2021-01-07 10:52:32 -0800

    test(cypress): Add E2E tests for drawing annotations on images

--- test/integration/Drawing.e2e.test.js
+++ test/integration/Drawing.e2e.test.js
@@ -73,4 +73,66 @@ describe('Drawing', () => {
 
         cy.get('.ba-DrawingTarget').should('have.class', 'is-active');
     });
+
+    it('should create a new drawing on an image', () => {
+        // Show the preview
+        cy.showPreview(Cypress.env('FILE_ID_IMAGE'));
+
+        // Wait for the empty drawing layer to be present
+        cy.getByTestId('ba-Layer--drawing');
+
+        // Assert that the drawing creator does not exist and no annotations are present
+        cy.getByTestId('ba-DrawingCreator').should('not.exist');
+        cy.get('.ba-DrawingTarget').should('not.exist');
+
+        // Enter drawing creation mode
+        cy.getByTestId('bp-AnnotationsControls-drawBtn').click();
+
+        // Add a drawing annotation on the image
+        cy.drawStroke();
+        // Support multiple draws
+        cy.drawStroke({ x: 300 });
+        cy.getByTestId('ba-PopupDrawingToolbar-comment').click();
+        cy.submitReply();
+
+        // Assert that at least one annotation is present on the image and is active
+        cy.get('.ba-DrawingTarget').should('have.class', 'is-active');
+    });
+
+    it('should hide drawing button for rotated image', () => {
+        // Show the preview
+        cy.showPreview(Cypress.env('FILE_ID_IMAGE'));
+
+        // Assert drawing button is not hidden
+        cy.getByTestId('bp-AnnotationsControls-drawBtn')
+            .should('be.visible')
+            .click();
+
+        // Add a drawing annotation on the image
+        cy.drawStroke();
+        cy.getByTestId('ba-PopupDrawingToolbar-comment').click();
+        cy.submitReply();
+
+        // Assert that at least one annotation is present on the image
+        cy.get('.ba-DrawingTarget').should('be.visible');
+
+        // Rotate image
+        cy.getByTitle('Rotate left').click();
+
+        // Assert drawing button is hidden
+        cy.getByTestId('bp-AnnotationsControls-drawBtn').should('not.be.visible');
+        // Assert that drawing annotations are still visible after rotation
+        cy.get('.ba-DrawingTarget').should('be.visible');
+
+        // Rotate image back to non-rotated state
+        cy.getByTitle('Rotate left')
+            .click()
+            .click()
+            .click();
+
+        // Assert drawing button is not hidden
+        cy.getByTestId('bp-AnnotationsControls-drawBtn').should('be.visible');
+        // Assert that drawing annotations are still visible after rotation
+        cy.get('.ba-DrawingTarget').should('be.visible');
+    });
 });
