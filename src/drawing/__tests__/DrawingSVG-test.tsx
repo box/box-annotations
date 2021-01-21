5e4cc570d410d4299123168fdca3c62c51796e24
Jack Chen <yuxi.chen@berkeley.edu>
2021-01-21 14:04:38 -0800

    fix(drawing): mock useRef

--- src/drawing/__tests__/DrawingSVG-test.tsx
+++ src/drawing/__tests__/DrawingSVG-test.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import DrawingSVG from '../DrawingSVG';
 
+jest.mock('react', () => ({
+    ...jest.requireActual('react'),
+    useRef: jest.fn().mockReturnValue({ current: 'ba-DrawingSVG-shadow_123' }),
+}));
+
 describe('DrawingSVG', () => {
     const Component = (): JSX.Element => <div>Test</div>;
     const getWrapper = (props = {}): ShallowWrapper =>
@@ -16,7 +21,7 @@ describe('DrawingSVG', () => {
             const wrapper = getWrapper();
 
             expect(wrapper.hasClass('ba-DrawingSVG')).toBe(true);
-            expect(wrapper.find('filter').prop('id')).toBe('ba-DrawingSVG-shadow');
+            expect(wrapper.find('filter').prop('id')).toBe('ba-DrawingSVG-shadow_123');
             expect(wrapper.exists('feGaussianBlur')).toBe(true);
             expect(wrapper.exists(Component)).toBe(true);
         });
